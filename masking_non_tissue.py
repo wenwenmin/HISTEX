17db55104efeda1675c01f2bd3b6717908fdc56a
shuailinxue <144300347+shuailinxue@users.noreply.github.com>
2025-02-21 21:17:07 +0800

    Add files via upload

--- masking_non_tissue.py
+++ masking_non_tissue.py
@@ -125,8 +125,8 @@ def main(prefix):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Masking of non-tissue areas.")
-    parser.add_argument('prefix', type=str, default='')
+    parser.add_argument(' --directory', type=str, default='')
 
     args = parser.parse_args()
 
-    main(args.prefix)
+    main(args.directory)
